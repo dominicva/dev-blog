ddb61327279630fca00e677ce1f4094fa67490fb
Dom van Almsick <dominicvana@gmail.com>
2022-05-24 18:51:06 -0500

    Add blog section to home

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,25 +3,26 @@ import Image from 'next/image';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-function Header({ title }) {
-  return (
-    <header>
-      <Image src="/images/profile.png" alt="Dominic" width={150} height={150} />
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
 
-      <h1>{title ? title : 'Default title'}</h1>
-    </header>
-  );
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
 
-function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
-        {/* <Header title="My blog" /> */}
         <p>Hi, I'm Dom</p>
         <p>I like JS</p>
         Check out my first blog{' '}
@@ -29,8 +30,21 @@ function Home() {
           <a>post</a>
         </Link>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
-
-export default Home;
